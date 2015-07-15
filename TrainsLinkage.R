8005fe2d0b7ec013eb15ecd77095164dd288219d
Eiman Ahmed <eimanahmed96@gmail.com>
2015-07-15 15:55:22 -0400

    Updated the R Script file that produces TrainTravel.csv file

--- TrainsLinkage.R
+++ TrainsLinkage.R
@@ -67,5 +67,8 @@ trains_info<- trains_info[complete.cases(trains_info),]
 trains_info <- data.frame(trains_info[,c(1,6,2,7,3,4)])
 names(trains_info) <- c("Train","Train_Stop","Train_Stop2","From_Station","To_Station","Travel_Time")
 
+#Debug - convert from secs to numbers
+trains_info$Travel_Time <- as.numeric(trains_info$Travel_Time)
+
 #Export as R file - change the dir/file name per needs
 write.table(trains_info, "/home/ewahmed/subway-flow/TrainTravel.csv", sep=",") 
