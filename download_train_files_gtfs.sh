f8c09dd3f31eaed23ecda8d69872be1ffc5f0747
Eiman Ahmed <eimanahmed96@gmail.com>
2015-07-15 14:02:03 -0400

    Updated Download Shell Script to download Subway Data

--- download_train_files_gtfs.sh
+++ download_train_files_gtfs.sh
@@ -10,24 +10,20 @@
 #author:eiman ahmed
 #template developed by jake hofman
 
-#make directory to store folders
-cd /home/
-
-#makes directory to download files into
-mkdir GTFSData
-
 # set current directory as place to install zip file 
 DATA_DIR=.
 
-#url to get info from 
 #downloads a zip file full of files we need
 wget "http://web.mta.info/developers/data/nyct/subway/google_transit.zip"
 
 #change to directory of data
-cd /home/GTFSData
+cd $DATA_DIR
+
+#makes directory to download files into
+mkdir GTFSData
 
 # unzip and get all files need to run the GTFS R Scripts
-unzip google_transit.zip 
+unzip google_transit.zip -d GTFSData
 
 
 
