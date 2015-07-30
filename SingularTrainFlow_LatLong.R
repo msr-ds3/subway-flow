c3afc391614c7adc6173d8cd7dbfc0cd7c1fb282
Eiman Ahmed <eimanahmed96@gmail.com>
2015-07-30 16:27:11 -0400

    R Script to create SingulraTrainFlowLatLon.csv

--- SingularTrainFlow_LatLong.R
+++ SingularTrainFlow_LatLong.R
@@ -16,4 +16,12 @@ stops<- unique(subset(stops,select=c("stop_id","stop_lat","stop_lon")))
 
 stops_flow <- inner_join(flow,stops)
 
+#taking only the first lat long for each station
+firstids <- stops_flow %>% group_by(station_id) %>% summarise(stop_lat=first(stop_lat),stop_lon=first(stop_lon))
+stops_flow$stop_lat= NULL
+stops_flow$stop_lon=NULL
+stops_flow <- inner_join(stops_flow,firstids,by="station_id")
+
+stops_flow <- subset(stops_flow,select=c("train","train_stop","station_id","station","time_travel","line_name","stop_lat","stop_lon"))
+
 write.csv(stops_flow,"/home/ewahmed/subway-flow/SingularTrainFlowLatLon.csv",quote=FALSE)
