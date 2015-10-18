e163750610f6ba35c0db997cd1c9e6f0ba03ed54
Riva Tropp <riva.tropp@mail.yu.edu>
2015-10-17 22:53:05 -0400

    a_names_script_v2.py

--- join.sh
+++ join.sh
@@ -1,26 +1,22 @@
+#!/bin/bash
 #1:Take turnstile data:
-#turnstile_141018.txt
-#turnstile_150711.txt
-#cd ./MergingData
-rm ./turnstile_150711.txt
-cp ../turnstile_data/turnstile_150711.txt ./turnstile_150711.txt
-cat ../turnstile_data/turnstile_141018.txt >> turnstile_150711.txt
 
-awk -f pp_turnstile.awk > ./ts1.txt
+cp turnstile_data/turnstile_150711.txt ./turnstile_150711.txt
+cat turnstile_data/turnstile_141018.txt >> turnstile_150711.txt
 
-cat ts1.txt | sort | uniq > ts2.txt
+awk -f pp_turnstile.awk |  sort | uniq > ts2.txt
 
 #5: GTFS Data:
 #stops.txt
-awk -f pp_namesformerge.awk > stops1.txt
-cat stops1.txt | cut -d, -f2 | sort | uniq > stops2.txt
+awk -f pp_namesformerge.awk | cut -d, -f2 | sort | uniq > stops2.txt
 #8: (rm stops1.txt, rm stops2.txt)
 
 python a_names_script_v2.py #(It goes to matchtable.txt)
 awk -f takethetop.awk #(goes to unmatchedwords.txt and easymatches.txt)
 awk -f smalledits.awk #(goes to readyformerge.txt)
 
-cd ../
+rm ./turnstile_150711.txt 
+
 #12: Eiman's file:
 #../GoogleLineNames.csv
 
